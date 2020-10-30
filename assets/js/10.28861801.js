dbef3076a98e874793208594a262cd14bc8e6ffe
tangentyh <tangentyh@users.noreply.github.com>
2020-10-30 14:03:50 +0000

    deploy: 14866c995f05898c706f0fc7a907f33b7bf68eee

---
+++ assets/js/10.28861801.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{203:function(s,t,n){"use strict";n.r(t);var e={data:()=>({msg:"Hello this is <demo-component>"})},i=n(0),l=Object(i.a)(e,(function(){var s=this.$createElement;return(this._self._c||s)("p",{staticClass:"demo"},[this._v("\n  "+this._s(this.msg)+"\n")])}),[],!1,null,null,null);t.default=l.exports}}]);
